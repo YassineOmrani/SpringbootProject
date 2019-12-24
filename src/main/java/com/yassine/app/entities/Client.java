46dfa4fc19ad41b54f5baeb304c5b2092b889cd8
yacin235 <yacinnyassine@gmail.com>
2019-12-24 16:06:31 +0100

    Modified Client entity

--- src/main/java/com/yassine/app/entities/Client.java
+++ src/main/java/com/yassine/app/entities/Client.java
@@ -1,5 +1,13 @@
 package com.yassine.app.entities;
 
-public class Client {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.Table;
+
+@Entity
+
+public class Client implements Serializable {
+	private float balbala;
 }
