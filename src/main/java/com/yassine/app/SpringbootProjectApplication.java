ffcd07e57dfc16fc1dd133de35a0a02ddd0810cd
yacin235 <yacinnyassine@gmail.com>
2019-12-24 14:00:51 +0100

    init project

---
+++ src/main/java/com/yassine/app/SpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yassine.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+
+}
